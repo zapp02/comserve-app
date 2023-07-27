2963c67b50392200770cf00f093504dd2ce9912a
zapp02 <verdiantoalgonz@gmail.com>
2023-07-27 16:33:00 +0700

    create dyanmic checkout page

--- resources/views/home/checkout.blade.php
+++ resources/views/home/checkout.blade.php
@@ -5,82 +5,68 @@
 @section('content')
     <!-- Checkout Start -->
     <div class="container-fluid pt-5">
-        <div class="row px-xl-5">
-            <div class="col-lg-2">
-                
-            </div>
-            <div class="col-lg-5">
-                <div class="card border-secondary mb-5">
-                    <div class="card-header bg-secondary border-0">
-                        <h4 class="font-weight-semi-bold m-0">Order Total</h4>
+            <div class="row px-xl-5">
+                <div class="col-lg-2">
+                </div>
+                <div class="col-lg-5">
+                    <div class="card border-secondary mb-5">
+                        <div class="card-header bg-secondary border-0">
+                            <h4 class="font-weight-semi-bold m-0">Order Details</h4>
+                        </div>
+                        
+                        <div class="card-footer border-secondary bg-transparent">
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-medium">Member name</h5>
+                                <h5 class="font-weight-medium">{{ $orders->member->member_name }} </h5>
+                            </div>
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-medium">Binusian ID</h5>
+                                <h5 class="font-weight-medium">{{ $orders->member->id_binusian }} </h5>
+                            </div>
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-medium">Major</h5>
+                                <h5 class="font-weight-medium">{{ $orders->member->major }} </h5>
+                            </div>
+                            <br>
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-medium">Request Total</h5>
+                                <h5 class="font-weight-medium">{{ $orders->order_total }} Pts</h5>
+                            </div>
+                            <div class="d-flex justify-content-between mt-2">
+                                <h5 class="font-weight-medium">Invoice</h5>
+                                <h5 class="font-weight-medium">{{ $orders->invoice }} </h5>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="font-weight-medium mb-3">Products</h5>
-                        <div class="d-flex justify-content-between">
-                            <p>Colorful Stylish Shirt 1</p>
-                            <p>qty here</p>
-                            <p>pts here</p>
+                </div>
+                <div class="col-lg-4 card border-secondary mb-5">
+                    <form name="checkout" class="checkout" method="POST" action="/payments">
+                        @csrf
+                        <input type="hidden" name="id_order" value="{{ $orders->id }}">
+
+                        <div class="card-header bg-secondary border-1">
+                            <h4 class="font-weight-semi-bold m-0">Payment</h4>
                         </div>
-                        <div class="d-flex justify-content-between">
-                            <p>Colorful Stylish Shirt 2</p>
-                            <p>qty here</p>
-                            <p>pts here</p>
+                        <div class="card-body">
+                            <br>
+                            <div class=" border-secondary bg-transparent">
+                                <div class="d-flex justify-content-between mt-2">
+                                    <h6 class="font-weight">After placing request, you are required to meet the requirements (if any) for each activity in order to be approved.</h6>
+                                </div>
+                            </div>
+                            <div class=" border-secondary bg-transparent">
+                                <div class="d-flex justify-content-between mt-2">
+                                    <h6 class="font-weight">If there any difficulties please, contact the authorities.</h6>
+                                </div>
+                            </div>
                         </div>
-                        <div class="d-flex justify-content-between">
-                            <p>Colorful Stylish Shirt 3</p>
-                            <p>qty here</p>
-                            <p>pts here</p>
+                        <div>
+                            <input type="submit" name="checkout_place_request" class="text-light btn btn-block btn-primary my-3 py-3 checkout" id="place_request" value="Place Request">
                         </div>
-                    </div>
-                    <div class="card-footer border-secondary bg-transparent">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5 class="font-weight-bold">Total</h5>
-                            <h5 class="font-weight-bold">$160</h5>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
-            <div class="col-lg-4 card border-secondary mb-5">
-                <div class="card-header bg-secondary border-1">
-                    <h4 class="font-weight-semi-bold m-0">Payment</h4>
-                </div>
-                <div class="card-body">
-                    <div class="form-group">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" class="custom-control-input" name="payment" id="paypal">
-                            <label class="custom-control-label" for="paypal">Paypal</label>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" class="custom-control-input" name="payment" id="directcheck">
-                            <label class="custom-control-label" for="directcheck">Direct Check</label>
-                        </div>
-                    </div>
-                    <div class="">
-                        <div class="custom-control custom-radio">
-                            <input type="radio" class="custom-control-input" name="payment" id="banktransfer">
-                            <label class="custom-control-label" for="banktransfer">Bank Transfer</label>
-                        </div>
-                    </div>
-                    <br>
-                    <div class=" border-secondary bg-transparent">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h6 class="font-weight">After placing request, you are required to meet the requirements (if any) for each activity in order to be approved.</h6>
-                        </div>
-                    </div>
-                    <div class=" border-secondary bg-transparent">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h6 class="font-weight">If there any difficulties please, contact the authorities.</h6>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <a href="" class="text-light btn btn-block btn-primary my-3 py-3">
-                        Place Request</a>
-                </div>
-            </div>
-        </div>
+        </form>
     </div>
     <!-- Checkout End -->
 @endsection
