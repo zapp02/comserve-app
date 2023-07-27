1a2c9060b11efffeffcff1aeecfbb158c99231a4
zapp02 <verdiantoalgonz@gmail.com>
2023-07-27 16:38:00 +0700

    create orders page to view user orders progress

---
+++ resources/views/home/orders.blade.php
@@ -0,0 +1,62 @@
+@extends('layout.home')
+
+@section('title', 'My Payments and Order')
+
+@section('content')
+    <!-- Checkout Start -->
+    <div class="container-fluid pt-5">
+        <div class="row px-xl-5">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-5">
+                
+                <h2>My Payments</h2>
+                <table class="table table-ordered table-hover table-striped">
+                    <thead>
+                        <th>No</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($payments as $index => $payment)
+                            <tr>
+                                <td>{{ $index+1 }}</td>
+                                <td>{{ $payment->created_at }}</td>
+                                <td>{{ $payment->status }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <br>
+                <h2>My Orders</h2>
+                <table class="table table-ordered table-hover table-striped">
+                    <thead>
+                        <th>No</th>
+                        <th>Date</th>
+                        <th>Request Total</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($orders as $index => $order)
+                            <tr>
+                                <td>{{ $index+1 }}</td>
+                                <td>{{ $order->created_at }}</td>
+                                <td>{{ $order->order_total }}</td>
+                                <td>{{ $order->status }}</td>
+                                <td>
+                                    <form action="/order_done/{{ $order->id }}" method="POST">
+                                        @csrf
+                                        <button type="submit" class="btn btn-success">DONE</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+    <!-- Checkout End -->
+@endsection
